6a7fca779adf671d66542ec0597487c24df879b7
Joanne Reece <joanne.reece@natwest.com>
2023-09-29 15:31:53 +0100

    Initial commit

---
+++ src/test/java/com/example/CyptoProject/CyptoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CyptoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyptoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
